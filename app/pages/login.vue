36989a30302cbc58d19d02ce2f48050a824b4521
Anilson Lopes <nissobmx@gmail.com>
2026-05-31 12:25:10 -0300

    add supabase

---
+++ app/pages/login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const email = ref('')
+
+async function signInWithOtp() {
+  await supabase.auth.signInWithOtp({
+    email: email.value
+  })
+}
+</script>
+
+<template>
+  <input type="email" v-model="email" @keyup.enter="signInWithOtp()" placeholder="Login with email" />
+</template>
