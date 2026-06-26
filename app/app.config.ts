5c3fcd4ba1daf52b36c35f2a7008e88fbe6827b6
Anilson Lopes <nissobmx@gmail.com>
2026-06-26 18:14:13 -0300

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'red',
-      neutral: 'zinc'
-    }
-  }
-})
+      primary: "yellow",
+      neutral: "mist",
+    },
+  },
+});
